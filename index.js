1b20d13c774599884f521bdf182e8a582004c4c9
Jatin Kumar <jatin15011999@gmail.com>
2019-09-25 10:47:03 +0200

    hamburger improved

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let $sidebar_toggle_button = document.getElementById("sidebar-toggle-btn");
+let $menu_toggle_button = document.getElementById("menu-toggle-btn");
 let $m_sidebar = document.querySelector(".m-sidebar");
 
 const handleSidebarToggleBtn = function(){
   console.log($m_sidebar.style.transform);
   $m_sidebar.classList.toggle("move_m_sidebar_up");
-  $sidebar_toggle_button.classList.toggle("sidebar-toggle-btn-clicked");
-}
+  $menu_toggle_button.classList.toggle("is-active");
+} 
