80f0cf5f90b1db3f73ddf5eb4128a66da5298d9a
Akshay Dahiya <xadahiya@gmail.com>
2019-12-21 11:02:52 +0530

    cleanup and add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+let $menu_toggle_button = document.getElementById("menu-toggle-btn");
+let $m_sidebar = document.querySelector(".m-sidebar");
+
+const handleSidebarToggleBtn = function(){
+  console.log($m_sidebar.style.transform);
+  $m_sidebar.classList.toggle("move_m_sidebar_up");
+  $menu_toggle_button.classList.toggle("is-active");
+} 
+
